f339164096bd4aa100d121f7246c1e813a3df939
zer0da <zer0day98@hotmail.com>
2021-10-23 02:38:37 +0300

    Update subEnum.sh

--- subEnum.sh
+++ subEnum.sh
@@ -4,7 +4,7 @@ read -p "Enter the path for the found subdomains output: " output
 
 cat "$input" | grep '*.' | sed -e 's/*.//' > /tmp/temp.txt | mv /tmp/temp.txt "$input"
 
-echo "The script started, please do not abort. It will find all subdomains for each domain! "
+echo "The script started, please do not abort. It will find all subdomains! "
 
 while IFS= read -r LINE || [[ -n "$LINE" ]]; do
     amass enum --passive -d "$LINE" >> "$output"
